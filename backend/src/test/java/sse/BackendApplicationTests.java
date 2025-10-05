7a61eceac1b4b702706e10ffe4ad56e8f6ada639
신관규 <soundbar91@gmail.com>
2025-10-05 12:09:13 +0900

    feat: 백엔드 프로젝트 세팅

---
+++ backend/src/test/java/sse/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
